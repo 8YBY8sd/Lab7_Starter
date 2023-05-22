abf8e384abad1116d3a18d209b5e0fd2bdd1d56b
8YBY8sd <b9yu@ucsd.edu>
2023-05-21 23:05:50 -0700

    explore

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
+  //We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
   // list to the cache as they come in.
